94ef94e762b6ee7d02d054e5f81ff65836fe0611
gerasymchuk <mgerasimchuk95@gmail.com>
2026-02-09 23:23:17 +0200

    feat: implement month-by-month iteration until current date

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.config import Config
 from src import csv_processor
 from src import helpers
 from src.sheets_manager import SheetsManager
+from datetime import datetime
 
 def main():
     config = Config()
@@ -10,13 +11,22 @@ def main():
 
     last_year, last_month = sheets_manager.get_last_processed_month()
     target_year, target_month = helpers.calc_next_month(last_year, last_month)
+    current_date = helpers.current_month_date()
 
+    last_processed_year = None
+    last_processed_month = None
 
-    rows = csv_processor.process_expenses(config.CSV_PATH, target_month, target_year)
+    while helpers.to_date(target_year, target_month) < current_date:
+        print(f'Processing {target_year} {target_month}')
+        rows = csv_processor.process_expenses(config.CSV_PATH, target_month, target_year)
+        sheets_manager.insert_expense_rows(rows)
 
+        last_processed_year = target_year
+        last_processed_month = target_month
 
-    sheets_manager.insert_expense_rows(rows)
-    sheets_manager.update_last_processed_month(f'{target_year} {target_month}') # year month
+        target_year, target_month = helpers.calc_next_month(target_year, target_month)
+
+    sheets_manager.update_last_processed_month(f'{last_processed_year} {last_processed_month}') # year month
 
 if __name__ == '__main__':
     main()
