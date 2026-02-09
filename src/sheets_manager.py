e94643b24225785788a26f4dcc367b5e35f1ff72
gerasymchuk <mgerasimchuk95@gmail.com>
2026-02-09 13:44:49 +0200

    feat: update Sheets Manager class: - methods to insert income/expenses rows - get/update last proccesed month

--- src/sheets_manager.py
+++ src/sheets_manager.py
@@ -1,5 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import date
+from src.model import TransactionRow
 
 
 class SheetsManager:
@@ -8,7 +10,7 @@ class SheetsManager:
         self.credentials_path = credentials_path
         self.client = None
         self.sheet = None
-        self.connect()
+        self._connect()
 
     def _connect(self):
         scope = [
@@ -20,8 +22,28 @@ class SheetsManager:
         self.sheet = self.client.open_by_key(self.sheet_id)
 
 
-    def get_sheet(self):
-        return self.sheet
+    def insert_expense_rows(self, rows: list[TransactionRow]) -> None:
+        if not rows:
+            return
 
-    def get_sheet_by_name(self, sheet_name: str):
-        return self.sheet.worksheet(sheet_name)
+        ws = self.sheet.worksheet('Витрати')
+        rows = [row.to_list() for row in rows]
+        ws.insert_rows(rows, row=2, value_input_option='USER_ENTERED')
+
+    def insert_income_rows(self, rows: list[TransactionRow]) -> None:
+        if not rows:
+            return
+
+        ws = self.sheet.worksheet('Доходи')
+        rows = [row.to_list() for row in rows]
+        ws.insert_rows(rows, row=2)
+
+    def get_last_processed_month(self) -> tuple[str, str]:
+        ws = self.sheet.worksheet('metadata')
+        year_month = ws.get('B1')[0][0]
+        last_processed_month = tuple[str, str](year_month.split(' ')) # ('2026', 'January')
+        return last_processed_month
+
+    def update_last_processed_month(self, year_month: str) -> None:
+        ws = self.sheet.worksheet('metadata')
+        ws.update_acell('B1', year_month)
